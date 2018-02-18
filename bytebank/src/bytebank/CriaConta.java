7af0a1b13d9acc0d830ab6b0435fb073e98cef53
claytonlima <limaberaldi@gmail.com>
2018-02-18 20:35:06 -0300

    nova classe cliente

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -3,19 +3,21 @@ package bytebank;
 public class CriaConta {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
+
+		Cliente primeiroCliente = new Cliente();
+		primeiroCliente.nome = "Clayton";
+		primeiroCliente.cpf = "222.222.222.22";
+		primeiroCliente.profissao = "Programador";
+		primeiraConta.titular = primeiroCliente;
+		
 		primeiraConta.saldo = 200;
-		System.out.println(primeiraConta.saldo);
-		
 		primeiraConta.saldo += 100;
-		System.out.println(primeiraConta.saldo);
 		
-		Conta segundaConta = new Conta();
-		segundaConta.saldo = 50;
-		
-		System.out.println("Primeira conta: R$ "+ primeiraConta.saldo);
-		System.out.println("Segunda conta: R$ "+ segundaConta.saldo);
-		
-		System.out.println("Refeência primeira conta "+ primeiraConta);
-		System.out.println("Refeência segunda conta "+ segundaConta);
+		System.out.println("Cliente: "+primeiraConta.titular.nome);
+		System.out.println("CPF: "+primeiraConta.titular.cpf);
+		System.out.println("Profissão: "+primeiraConta.titular.profissao);
+		System.out.println("Saldo: "+primeiraConta.saldo);
+		System.out.println("Primeira conta: R$ " + primeiraConta.saldo);
+		System.out.println("Refeência primeira conta " + primeiraConta);
 	}
 }
