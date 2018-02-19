5119c2b9f1f902674d7d98118c903dd6618b3435
claytonlima <limaberaldi@gmail.com>
2018-02-18 23:54:31 -0300

    arrumando classe conta

--- bytebank/src/bytebank/Cliente.java
+++ bytebank/src/bytebank/Cliente.java
@@ -4,4 +4,28 @@ public class Cliente {
 	String	nome;
 	String	cpf;
 	String	profissao;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public String getProfissao() {
+		return profissao;
+	}
+	
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
 }
--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -3,21 +3,24 @@ package bytebank;
 public class CriaConta {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
+		primeiraConta.setAgencia(2788);
+		primeiraConta.setNumero(10408);
 
 		Cliente primeiroCliente = new Cliente();
 		primeiroCliente.nome = "Clayton";
 		primeiroCliente.cpf = "222.222.222.22";
 		primeiroCliente.profissao = "Programador";
-		primeiraConta.titular = primeiroCliente;
+		primeiraConta.setTitular(primeiroCliente);
 		
-		primeiraConta.saldo = 200;
-		primeiraConta.saldo += 100;
+		primeiraConta.deposita(200);
+		primeiraConta.deposita(100);
 		
-		System.out.println("Cliente: "+primeiraConta.titular.nome);
-		System.out.println("CPF: "+primeiraConta.titular.cpf);
-		System.out.println("Profissão: "+primeiraConta.titular.profissao);
-		System.out.println("Saldo: "+primeiraConta.saldo);
-		System.out.println("Primeira conta: R$ " + primeiraConta.saldo);
+		System.out.println("Agência: "+primeiraConta.getAgencia());
+		System.out.println("Conta: "+primeiraConta.getNumero());
+		System.out.println("Cliente: "+primeiraConta.getTitular().getNome());
+		System.out.println("CPF: "+primeiraConta.getTitular().getCpf());
+		System.out.println("Profissão: "+primeiraConta.getTitular().getProfissao());
+		System.out.println("Primeira conta: R$ " + primeiraConta.getSaldo());
 		System.out.println("Refeência primeira conta " + primeiraConta);
 	}
 }
