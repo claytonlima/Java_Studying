ef583b36763757309ba360218af62c8b60f6aad7
clayton <clayton.beraldi@jacotei.com.br>
2018-03-27 13:59:15 -0300

    classe para calculo de area de um circulo

--- interfaces/src/interfaces/Teste.java
+++ interfaces/src/interfaces/Teste.java
@@ -4,6 +4,8 @@ public class Teste {
 	public static void main(String[] args) {
 		AreaCalculavel a = new Retangulo(3, 2);
 		System.out.println(a.calculaArea());
+		
+		AreaCalculavel c = new Circulo(3);
+		System.out.println(c.calculaArea());
 	}
-
 }
